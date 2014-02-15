c2b90f1ce88b41aad454ca8299472a826fecdb1d
jimmy-gnahz <jimmyjimbozhang@gmail.com>
2014-02-14 19:09:52 -0800

    added key listener template code

--- src/ui/BSnake.java
+++ src/ui/BSnake.java
@@ -44,6 +44,7 @@ public class BSnake{
 		new BSnake();
 	}
 	
+	
 
 
 
--- src/ui/SnakeFrame.java
+++ src/ui/SnakeFrame.java
@@ -4,6 +4,8 @@ import java.awt.Dimension;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JFrame;
 import javax.swing.Timer;
@@ -19,7 +21,7 @@ public class SnakeFrame extends JFrame{
 	
 	/**
 	 * width and height of the window
-	 * TODO: You may wish to change these values
+	 * You may wish to change these values
 	 */
 	private int WIDTH = 500, HEIGHT = 500;
 	
@@ -42,7 +44,10 @@ public class SnakeFrame extends JFrame{
 		this.setLocation(x - WIDTH/2,y - HEIGHT/2);
 		setSize(WIDTH,HEIGHT);
 		setVisible(true);
+		
+		// Add timer and keyboard listener
 		addTimer();
+		addKeyListener(new KeyboardController());
 	}
 	
 	
@@ -60,5 +65,35 @@ public class SnakeFrame extends JFrame{
 		});
 		
 		t.start();
+	}
+	
+	private class KeyboardController implements KeyListener {
+		// TODO: implement each KeyEvent case.  You might need more KeyEvents.
+		@Override
+		public void keyPressed(KeyEvent e) {
+
+			switch (e.getKeyCode()) {
+			case KeyEvent.VK_LEFT: // Left
+				System.out.println("left key"); // These printouts are just for testing
+				break;
+			case KeyEvent.VK_UP: // Up
+				System.out.println("up key");
+				break;
+			case KeyEvent.VK_RIGHT: // Right
+				System.out.println("right key");
+				break;
+			case KeyEvent.VK_DOWN: // Down
+				System.out.println("down key");
+				break;
+			}
+		}
+
+		@Override
+		public void keyReleased(KeyEvent e) {
+		}
+
+		@Override
+		public void keyTyped(KeyEvent e) {
+		}
 	}
 }
